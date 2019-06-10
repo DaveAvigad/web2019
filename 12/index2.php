f1b306be34022df6a4e173ad95cf6e057c78d424
David Avigad <david@lusha.co>
2019-06-10 16:45:15 +0300

    remove secret files

--- 12/index2.php
+++ 12/index2.php
@@ -1,16 +1,6 @@
 <?php
     // Create a mySQL DB connection
-$dbhost = "182.50.133.168";
-$dbuser = "studDB19a";
-$dbpass = "stud19DB1!";
-$dbname = "studDB19a";
-
-    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-    // Testing connection success
-    if(mysqli_connect_errno()) {
-        die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
-    }
-
+    include 'db.php';
     ?>
 <!doctype html>
 <html lang="en">
